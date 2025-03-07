7a7fa74c6297ac7324ba556e1dc1cd155bc63837
Xavier Coulon <xcoulon@redhat.com>
2025-03-07 15:20:29 +0100

    use charmbracelet/log
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- main.go
+++ main.go
@@ -5,10 +5,10 @@
 package main
 
 import (
-	"log"
 	"math/rand"
 	"net/http"
 
+	"github.com/charmbracelet/log"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -21,26 +21,36 @@ func main() {
 	app.Get("/_health/ready", ready)
 	app.Get("/_health/alive", alive)
 
-	log.Print("starting chaotic-app")
+	log.Info("starting chaotic-app")
 	// Start server
 	log.Fatal(app.Listen(":3000"))
 }
 
 // `/hello` handler
 func hello(c *fiber.Ctx) error {
+	log.Infof("'hello' handler called by user-agent '%s'", getUserAgent(c))
 	return c.SendString("Hello, World 👋!")
 }
 
 // `/_health/ready` handler
 func ready(c *fiber.Ctx) error {
+	log.Infof("'ready' handler called by user-agent '%s'", getUserAgent(c))
 	return c.SendStatus(http.StatusOK)
 }
 
 // `/_health/alive` handler
 func alive(c *fiber.Ctx) error {
+	log.Infof("'alive' handler called by user-agent '%s'", getUserAgent(c))
 	// randomly alive
 	if rand.Intn(100) > 50 {
 		return c.SendStatus(http.StatusInternalServerError)
 	}
 	return c.SendStatus(http.StatusOK)
+}
+
+func getUserAgent(c *fiber.Ctx) string {
+	if ua, ok := c.GetReqHeaders()["User-Agent"]; ok && len(ua) > 0 {
+		return ua[0]
+	}
+	return "unknown"
 }
